4fc50fee811a217cdc1f1fc9c3ad57b14bb819ba
kiranb555 <kiran.basavaraj05@gmail.com>
2025-09-15 23:32:18 +0530

    initial commit testing basic app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="app">
+      <h1 style={{color: 'red'}}>Bun + React App</h1>
+    </div>
+  );
+}
+
+export default App;
